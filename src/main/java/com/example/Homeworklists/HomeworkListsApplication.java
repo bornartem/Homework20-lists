c7736d4bf1c08a538c6a418a7d7aea3a67eaa7ee
bornartem <born.artem@gmail.com>
2023-10-09 16:39:54 +0300

    1 commit

---
+++ src/main/java/com/example/Homeworklists/HomeworkListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Homeworklists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkListsApplication.class, args);
+	}
+
+}
